988f6aefe69b4dc6e38212931bca97ab7dde506b
Anna <anna.nester@gmail.com>
2018-06-11 09:37:14 -0300

    Criando Class Library e classes genéricas

---
+++ Hospital/Modelos/Medico.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Modelos
+{
+    public class Medico : Pessoa
+    {
+        [Key]
+        int crm { get; set; }
+
+        string especialidade { get; set; }
+    }
+}
---
+++ Hospital/Modelos/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Modelos
+{
+    public class Pessoa
+    {
+        [Required]
+        string nome { get; set; }
+
+        string endereco { get; set; }
+
+        string datanasc { get; set; }
+    }
+}
